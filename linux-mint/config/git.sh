cf2d62abb13aa94e17f3b40970775546d55415a3
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2016-07-04 00:37:41 +0200

    Fix git config

--- linux-mint/config/git.sh
+++ linux-mint/config/git.sh
@@ -3,6 +3,6 @@ set -e
 
 printf "Configuring git...\n"
 
-git config --global user.name = "Evert Geldenhuys"
-git config --global user.email = "evert.geldenhuys2@gmail.com"
+git config --global --replace-all user.name "Evert Geldenhuys"
+git config --global --replace-all user.email evert.geldenhuys2@gmail.com
 git config --global credential.helper 'cache --timeout=3600'
