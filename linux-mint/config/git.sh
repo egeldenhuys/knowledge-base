423a27a0522700b638a3b7567c125cfcc502bdc3
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2016-07-04 15:29:28 +0200

    Move config to dotfiles repo

--- linux-mint/config/git.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-printf "Configuring git...\n"
-
-git config --global --replace-all user.name "Evert Geldenhuys"
-git config --global --replace-all user.email evert.geldenhuys2@gmail.com
-git config --global credential.helper 'cache --timeout=3600'
